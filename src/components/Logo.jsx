1215250f0be75673bcd17857e41c9f0b3c9eddd6
w-alt-m <166965462+w-alt-m@users.noreply.github.com>
2025-12-21 08:44:19 -0300

    Add icon to Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,12 @@
+import { FaDog } from "react-icons/fa6";
+
 const Logo = () => {
-  return <a className="btn btn-ghost text-xl">Petshop Perritos</a>;
+  return (
+    <a className="btn btn-ghost text-xl">
+      <FaDog />
+      Petshop Perritos
+    </a>
+  );
 };
 
 export default Logo;
