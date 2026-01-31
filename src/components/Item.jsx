28901a8f6c8840d7a34deaf69b0b8cd36f36d074
w-alt-m <166965462+w-alt-m@users.noreply.github.com>
2026-01-31 13:27:42 -0300

    Create basic Item layout and install react-router-dom

---
+++ src/components/Item.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Item = ({ producto }) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={producto.img} />
+      <Card.Body>
+        <Card.Title>{producto.name}</Card.Title>
+        <Card.Text>${producto.price},00</Card.Text>
+        {/* <Link to={'/item/'+producto.id} className='btn btn-dark'>Ver Más</Link> */}
+        <Link to={`/item/${producto.id}`} className="btn btn-dark">
+          Ver Más
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Item;
