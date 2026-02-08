f04c52cf648480321832d7ea86c74c4a9be3b59c
w-alt-m <166965462+w-alt-m@users.noreply.github.com>
2026-02-08 01:28:00 -0300

    Add context to components

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import Navbar from "./components/Navbar.jsx";
 import ItemListContainer from "./components/ItemListContainer.jsx";
 import ItemDetailContainer from "./components/ItemDetailContainer.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Error from './components/Error';
+import Error from "./components/Error";
+// importar proveedor
+import { CartProvider } from "./context/CartContext";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<ItemListContainer />} />
-        <Route path='/category/:type' element={<ItemListContainer />} />
-        <Route path='/item/:id' element={<ItemDetailContainer />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
+      <CartProvider>
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:type" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </CartProvider>
     </BrowserRouter>
   );
 }
