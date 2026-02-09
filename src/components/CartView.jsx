568a33e7cd521745086d0e130ff1a75c926e8e63
w-alt-m <166965462+w-alt-m@users.noreply.github.com>
2026-02-08 22:27:52 -0300

    Finish Cart basic layout

--- src/components/CartView.jsx
+++ src/components/CartView.jsx
@@ -9,11 +9,20 @@ const CartView = () => {
       <div>
         {cart.map((prod) => (
           <div key={prod.id}>
+            <img src={prod.img} alt={prod.name} />
             <h2>{prod.name}</h2>
-            <p>{prod.price}</p>
+            <p>Precio por unidad ${prod.price}</p>
+            <p>Cantidad: {prod.qty}</p>
+            <p>Subtotal: ${prod.price * prod.qty}</p>
             <button onClick={() => removeItem(prod.id)}>Eliminar</button>
           </div>
         ))}
+      </div>
+      {/* llamar funcion en contexto q devuelva el resultado total */}
+      <p>Total: ${total}</p>
+      <div>
+        <button onClick={clear}>Vaciar carrito</button>
+        <button onClick={clear}>Finalizar compra</button>
       </div>
     </div>
   );
