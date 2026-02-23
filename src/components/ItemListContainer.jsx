8a642e306a0af76b7d88ad1beb74cc843786bf10
w-alt-m <166965462+w-alt-m@users.noreply.github.com>
2026-02-22 22:17:12 -0300

    Add ItemDetailContainer Firebase Implementation

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
-/* import { getProducts } from "../asyncMock/data"; */
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
 import Loader from "./Loader";
-import { collection, getDocs, where } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../service/firebase";
 
 const ItemListContainer = () => {
@@ -29,21 +28,6 @@ const ItemListContainer = () => {
       .catch((error) => console.log(error))
       .finally(() => setLoading(false));
   }, [type]);
-
-  // ASYNC MOCK
-  /*   useEffect(() => {
-    setLoading(true);
-    getProducts()
-      .then((res) => {
-        if (type) {
-          setData(res.filter((prod) => prod.category === type));
-        } else {
-          setData(res);
-        }
-      })
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
-  }, [type]); */
 
   return <>{loading ? <Loader /> : <ItemList data={data} />}</>;
 };
