2dd841d83c6d3fa2442408b8d4dfbd53c05bdde8
w-alt-m <166965462+w-alt-m@users.noreply.github.com>
2026-02-22 21:46:46 -0300

    Testing firebase implementation

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import { getProducts } from "../asyncMock/data";
+/* import { getProducts } from "../asyncMock/data"; */
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
 import Loader from "./Loader";
+import { collection, getDocs, where } from "firebase/firestore";
+import { db } from "../service/firebase";
 
 const ItemListContainer = () => {
   const [data, setData] = useState([]);
@@ -10,6 +12,26 @@ const ItemListContainer = () => {
   const { type } = useParams();
 
   useEffect(() => {
+    setLoading(true);
+    const prodCollection = type
+      ? query(collection(db, "productos"), where("category", "==", type))
+      : collection(db, "productos");
+    getDocs(prodCollection)
+      .then((res) => {
+        const list = res.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setData(list);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [type]);
+
+  // ASYNC MOCK
+  /*   useEffect(() => {
     setLoading(true);
     getProducts()
       .then((res) => {
@@ -21,7 +43,7 @@ const ItemListContainer = () => {
       })
       .catch((error) => console.log(error))
       .finally(() => setLoading(false));
-  }, [type]);
+  }, [type]); */
 
   return <>{loading ? <Loader /> : <ItemList data={data} />}</>;
 };
--- src/service/firebase.jsx
+++ src/service/firebase.jsx
@@ -1,9 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -15,3 +13,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Obtain Database from Firestore
+export const db = getFirestore(app);
